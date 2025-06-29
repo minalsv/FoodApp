aac09cbdf3c6b01437babdcc6ec2ba68b4f366ea
minalsv <57864362+minalsv@users.noreply.github.com>
2025-06-29 15:22:36 +0200

    Segregate code into appropriate modules

--- App.js
+++ App.js
@@ -4,45 +4,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import {Cell, Section, TableView} from 'react-native-tableview-simple';
+
+// Import Screens
+import RestaurantsScreen from './components/Restaurants';
+import MenuScreen from './components/Menu';
+import styles from './assets/styles/styles'
+
 const Stack = createStackNavigator();
-
-// Restaurants Screen component
-function RestaurantsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Restaurants Screen</Text>
-    </View>
-  );
-}
-
-// Menu Screen component
-function MenuScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Menu Screen</Text>
-    </View>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* Add the Restaurants screen */}
         <Stack.Screen name="Restaurants" component={RestaurantsScreen} />
-
-        {/* Add the Menu screen */}
         <Stack.Screen name="Menu" component={MenuScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
