0fc4f807e2cf6dae75a2f04a8d11280ed96dbe4e
minalsv <57864362+minalsv@users.noreply.github.com>
2025-06-29 12:32:53 +0200

    Intall few more packages table view, stack etc.

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        </View>
+    );
+}
+
+function SignUpScreen() {
+ return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>SignUpScreen</Text>
+      </View>
+ );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Sign up" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <TouchableOpacity
+        style={styles.signUp}
+        onPress={() => navigation.navigate("Sign up")}
+      >
+        <Text style={styles.signUpText}>Sign up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
